8239d6c909deb19b6899f0b34f9266480519327a
Valueva <139693392+Valueva@users.noreply.github.com>
2025-11-28 17:37:18 +0200

    add: homework_02

---
+++ src/components/Test1/Content1.jsx
@@ -0,0 +1,9 @@
+function Content1() {
+	return (
+		<div>
+			<img style={{ width: '100px' }} src="https://img.freepik.com/premium-vector/smiling-emoticon-with-smiling-eyes_1303870-79.jpg?semt=ais_hybrid&w=740&q=80" />
+		</div>
+	);
+}
+
+export default Content1;
---
+++ src/components/Test1/Content2.jsx
@@ -0,0 +1,12 @@
+function Content2() {
+
+
+
+	return (
+		<div>
+			помилка
+		</div>
+	);
+}
+
+export default Content2;
