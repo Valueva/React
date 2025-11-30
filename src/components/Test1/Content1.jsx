5b32a88760f48061986e3ddc003444473796f83a
Valueva <139693392+Valueva@users.noreply.github.com>
2025-11-30 22:00:04 +0200

    Delete src directory

--- src/components/Test1/Content1.jsx
+++
@@ -1,9 +0,0 @@
-function Content1() {
-	return (
-		<div>
-			<img style={{ width: '100px' }} src="https://img.freepik.com/premium-vector/smiling-emoticon-with-smiling-eyes_1303870-79.jpg?semt=ais_hybrid&w=740&q=80" />
-		</div>
-	);
-}
-
-export default Content1;
